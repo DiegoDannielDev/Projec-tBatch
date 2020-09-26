0cbbbcbd3fca6afe8e5741dd15469a339d815fb3
diego danniel <diego.diniz@dasa.com.br>
2020-09-26 11:47:33 -0300

    init comit update

---
+++ src/main/java/br/com/projectbatch/ProjetcbatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.projectbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetcbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetcbatchApplication.class, args);
+    }
+
+}
